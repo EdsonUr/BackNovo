ecb3b01b3fe1ffa32b3a037d4e82bf9cd9967030
Nosdeds <e.uranoc@gmail.com>
2023-10-02 14:19:25 -0300

    feat: unique animal

--- src/models/Animal.js
+++ src/models/Animal.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const AnimalSchema = new mongoose.Schema({
-    name: {type: String, required:true},
+    name: {type: String, required:true, unique:true},
     type: {type: String, required:true},
     userCodigo: {type:String, required: true},
 })
